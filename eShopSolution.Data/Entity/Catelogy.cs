c10d7384e8ebab68a3ee37930b792c271cca258e
doanh xuan <xuandoanh1997@gmail.com>
2020-06-03 11:55:06 +0700

    Add entity

---
+++ eShopSolution.Data/Entity/Catelogy.cs
@@ -0,0 +1,16 @@
+﻿using eShopSolution.Data.Enum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.Entity
+{
+    public class Catelogy
+    {
+       public int Id { set; get; }
+       public int SortOrder  { set; get; }
+       public bool IsShowOnHome  { set; get; }
+       public int ParenId  { set; get; }
+       public Status Status { set; get; }
+    }
+}
---
+++ eShopSolution.Data/Entity/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace eShopSolution.Data.Entity
+{
+    [Table("Products")]
+    public class Product
+    {
+        public int Id { get; set; }
+        public decimal Price { get; set; }
+        public decimal OriginalPrice { get; set; }
+        public int Stock{ get; set; }
+        public int ViewScount { get; set; }
+        public DateTime DateCroated { get; set; }
+        public string SeoAlias { get; set; }
+
+
+
+
+
+    }
+}
